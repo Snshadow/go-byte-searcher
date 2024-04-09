d94e9c26b9f4cabe5844748258489d3a1682e0cc
Snshadow <randy0007@naver.com>
2024-04-09 09:52:38 +0900

    feat: guess encoding type of file

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -9,8 +9,10 @@ import (
 	"golang.org/x/text/encoding/unicode"
 )
 
-// Get encoder and name of the used encoding based on its encoding type, currently supports UTF-8, UTF-8 with BOM, UTF-16LE, UTF-16BE.
-// The returned encoder will not add BOM at the start of the transformed buffer.
+// GetFileEncoder creates encoder with file encoding type and encoding name according to its encoding type, currently supports UTF-8, UTF-8 with BOM, UTF-16LE, UTF-16BE.
+// The returned encoder does not add BOM at the start of the transformed buffer.
+//
+// This function also tries to guess encoding type for some weird files with no BOM prepended(especially UTF-16LE and UTF-16BE files).
 func GetFileEncoder(file *os.File) (encoder *encoding.Encoder, encName string, err error) {
 	buf := make([]byte, 3) // BOM sequence is at maximum 3 bytes for utf-8
 	_, err = file.ReadAt(buf, 0)
@@ -18,31 +20,52 @@ func GetFileEncoder(file *os.File) (encoder *encoding.Encoder, encName string, e
 		return
 	}
 
-    var usedEnc encoding.Encoding
+	encNames := [...]string{"UTF-16LE", "UTF-16BE", "UTF-8", "UTF-8 with BOM", "Unknown"}
+	encName = encNames[4] // "Unknown"
 
-    // use IgnoreBOM to not insert BOM at start of buffer
-    if buf[0] == '\xff' && buf[1] == '\xfe' {
-        usedEnc = unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM)
-        encName = "UTF-16LE"
-    } else if buf[0] == '\xfe' && buf[1] == '\xff' {
-        usedEnc = unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM)
-        encName = "UTF-16BE"
-    } else if bytes.HasPrefix(buf, []byte("\ufeff")) {
-        usedEnc = unicode.UTF8 // use UTF-8 to not insert BOM at start of buffer
-        encName = "UTF-8 with BOM"
-    } else {
-        usedEnc = unicode.UTF8
-        encName = "UTF-8"
-    }
-    // TODO: add other encoding types..?
+	var usedEnc encoding.Encoding
 
-    if usedEnc == nil {
-        err = fmt.Errorf("could not get encoding type of given file")
-        encName = "Unknown"
-        return
-    }
+	// use IgnoreBOM to not insert BOM at start of buffer
+	encList := [...]encoding.Encoding{unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM), unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM), unicode.UTF8}
 
-    encoder = usedEnc.NewEncoder()
+	if buf[0] == '\xff' && buf[1] == '\xfe' {
+		usedEnc = encList[0]
+		encName = encNames[0] // "UTF-16LE"
+	} else if buf[0] == '\xfe' && buf[1] == '\xff' {
+		usedEnc = encList[1]
+		encName = encNames[1] // "UTF-16BE"
+	} else if bytes.HasPrefix(buf, []byte("\ufeff")) {
+		usedEnc = encList[2]  // use UTF-8 to not insert BOM at start of buffer
+		encName = encNames[3] // "UTF-8 with BOM"
+	} else {
+		// try decoding with some decoders
+		var decList []*encoding.Decoder
+		for _, e := range encList {
+			decList = append(decList, e.NewDecoder())
+		}
+		dBuf, resBuf := make([]byte, 4), make([]byte, 4) // utf-8 can use up to 4 bytes for currently used characters
+		_, err = file.ReadAt(dBuf, 0)
+		if err != nil {
+			return nil, encName, err
+		}
+		for i, dec := range decList {
+			_, _, err = dec.Transform(resBuf, dBuf, true)
+			if bytes.Count(resBuf, []byte{0}) != 4 && err == nil {
+				usedEnc = encList[i]
+				encName = encNames[i]
+				break
+			}
+		}
 
-    return
+	}
+	// TODO: add other encoding types..?
+
+	if usedEnc == nil {
+		err = fmt.Errorf("could not get encoding type of given file")
+		return
+	}
+
+	encoder = usedEnc.NewEncoder()
+
+	return
 }
--- searcher.go
+++ searcher.go
@@ -15,8 +15,8 @@ import (
 )
 
 type ByteSearcher struct {
-	File *os.File
-    EncType string // UTF-8, UTF-8 with BOM, UTF-16LE, UTF-16BE
+	File    *os.File
+	EncType string // UTF-8, UTF-8 with BOM, UTF-16LE, UTF-16BE
 
 	fileSize int64
 
@@ -24,26 +24,27 @@ type ByteSearcher struct {
 
 	isComplete *atomic.Bool
 
-	result SearchResult
+	result searchResult
 }
 
-type SearchResult struct {
+type searchResult struct {
 	mutex   *sync.Mutex
 	offsets []int
 }
 
-func (r *SearchResult) addResult(offset int) {
+func (r *searchResult) addResult(offset int) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 	r.offsets = append(r.offsets, offset)
 }
 
 // NewSearcher opens file descriptor and creates new byte searcher for a file in given path.
-// The file descriptor can be accessed with File field. Need to close file descriptor after use.
+// If isText is set to true, the encoder for a file will also be assigned, return error if file is not a text file.
+// The file descriptor can be accessed with File field. Needs to be closed after use.
 func NewSearcher(path string, isText bool) (ByteSearcher, error) {
 	newSearcher := ByteSearcher{
 		isComplete: &atomic.Bool{},
-		result: SearchResult{
+		result: searchResult{
 			mutex:   &sync.Mutex{},
 			offsets: []int{},
 		},
@@ -76,8 +77,8 @@ func NewSearcher(path string, isText bool) (ByteSearcher, error) {
 	return newSearcher, nil
 }
 
-// s.Search finds offsets of a given query from searched file.
-// If searchOne is set to true, searcher will search for one offset then return, it may be used if only one match exists in the file is guaranteed, 
+// Search finds offsets of a given query from searched file.
+// If searchOne is set to true, searcher will search for one offset then return, it may be used if only one match exists in the file is guaranteed,
 // runCount sets the number of concurrently run search sessions.
 func (s *ByteSearcher) Search(query []byte, searchOne bool, runCount ...uint32) (offsets []int, err error) {
 	var concur uint32 = 4 // default to 4 concurrent search
@@ -99,8 +100,8 @@ func (s *ByteSearcher) Search(query []byte, searchOne bool, runCount ...uint32) 
 
 	for session := 0; session < int(concur); session++ {
 		readBuf := make([]byte, querySize)
-        readOffset := runSize * int64(session)
-        session := session
+		readOffset := runSize * int64(session)
+		session := session
 		wg.Add(1)
 		go func(f *os.File, b []byte) {
 			sz := runSize
@@ -108,24 +109,24 @@ func (s *ByteSearcher) Search(query []byte, searchOne bool, runCount ...uint32) 
 				sz += lastRem
 			}
 			for i := 0; i < int(sz); i++ {
-                _, err := f.ReadAt(b, readOffset + int64(i))
-                if err == io.EOF {
-                    break
-                } else if err != nil {
-                    fmt.Printf("failed to read buffer from offset %d\n", i + int(readOffset))
-                    continue
-                }
+				_, err := f.ReadAt(b, readOffset+int64(i))
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					fmt.Printf("failed to read buffer from offset %d\n", i+int(readOffset))
+					continue
+				}
 
-                if bytes.Equal(b, query) {
-                    s.result.addResult(int(readOffset) + i)
-                    if searchOne {
-                        s.isComplete.Store(true)
-                    }
-                }
+				if bytes.Equal(b, query) {
+					s.result.addResult(int(readOffset) + i)
+					if searchOne {
+						s.isComplete.Store(true)
+					}
+				}
 
-                if s.isComplete.Load() {
-                    break
-                }
+				if s.isComplete.Load() {
+					break
+				}
 			}
 
 			wg.Done()
@@ -133,33 +134,38 @@ func (s *ByteSearcher) Search(query []byte, searchOne bool, runCount ...uint32) 
 	}
 	wg.Wait()
 
-    sort.SliceStable(s.result.offsets, func(i, j int) bool {
-        return s.result.offsets[i] < s.result.offsets[j]
-    })
+	sort.SliceStable(s.result.offsets, func(i, j int) bool {
+		return s.result.offsets[i] < s.result.offsets[j]
+	})
 	offsets = s.result.offsets
 
 	return
 }
 
-// s.SearchString searches for query string following the encoding type of file for searching.
-// See s.Search for other details.
+// SearchString searches for query string following the encoding type of file for searching.
+// See Search for other details.
 func (s *ByteSearcher) SearchString(query string, searchOne bool, runCount ...uint32) (offsets []int, err error) {
+	if s.encoder == nil {
+		err = fmt.Errorf("searcher was not initalized for text file")
+		return
+	}
+
 	var cr uint32 = 4 // default to 4 concurrent search
-    if len(runCount) != 0 {
-        cr = runCount[0]
-    }
+	if len(runCount) != 0 {
+		cr = runCount[0]
+	}
 
-    qBuf, err := s.encoder.Bytes([]byte(query))
-    if err != nil {
-        return
-    }
+	qBuf, err := s.encoder.Bytes([]byte(query))
+	if err != nil {
+		return
+	}
 
-    offsets, err = s.Search(qBuf, searchOne, cr)
+	offsets, err = s.Search(qBuf, searchOne, cr)
 
 	return
 }
 
-// s.Close closes the file descriptor of a searched file
+// Close closes the file descriptor of a searched file
 func (s *ByteSearcher) Close() {
-    s.File.Close()
+	s.File.Close()
 }
